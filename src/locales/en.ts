0415195cb67a3c6b77f8e69ce891e525a54ebe21
繁星 <3091977221@qq.com>
2024-04-04 00:14:01 +0800

    add locales

---
+++ src/locales/en.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    "Simplified Chinese": "Simplified Chinese",
+    "Traditional Chinese": "Traditional Chinese",
+    "English": "English",
+    "BaudRate": "BaudRate",         
+    "WordLength": "WordLength",
+    "StopBits": "StopBits",
+    "Parity": "Parity",   
+    "HardwareFlowControl": "HardwareFlowControl",
+    "Cache": "Cache",
+    "Options": "Options"
+}
---
+++ src/locales/zh.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    "Simplified Chinese": "简体中文",
+    "Traditional Chinese": "繁体中文",
+    "English": "英语",
+    "BaudRate": "波特率",         
+    "WordLength": "数据位",
+    "StopBits": "停止位",
+    "Parity": "校验位",   
+    "HardwareFlowControl": "硬件流控制在",
+    "Cache": "缓冲区",
+    "Options": "选项"
+}
