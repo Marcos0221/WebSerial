40ce339ebb20a0170116275105701ffc90cadeb6
Moieo <moe@moieo.cn>
2024-04-03 16:33:05 +0800

    对代码使用 Prettier 进行格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import "./App.css";
-import {
-  AppBar,
-  Box,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import MoreIcon from "@mui/icons-material/MoreVert";
 
@@ -14,24 +8,13 @@ function App() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             WebSerial
           </Typography>
-          <IconButton
-            size="large"
-            aria-label="display more actions"
-            edge="end"
-            color="inherit"
-          >
+          <IconButton size="large" aria-label="display more actions" edge="end" color="inherit">
             <MoreIcon />
           </IconButton>
         </Toolbar>
