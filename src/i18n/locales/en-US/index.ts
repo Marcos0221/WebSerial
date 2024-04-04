6a0dc07bc9588a5f20d7cc03adf544e07a2fdac9
繁星 <3091977221@qq.com>
2024-04-04 18:39:42 +0800

    新增国际化特性

---
+++ src/i18n/locales/en-US/index.ts
@@ -0,0 +1,17 @@
+const language = {
+    Simplified_Chinese: "Simplified Chinese:",
+    Traditional_Chinese: "Traditional Chinese",
+    English: "English",
+    BaudRate: "BaudRate",         
+    WordLength: "WordLength",
+    StopBits: "StopBits",
+    Parity: "Parity",   
+    HardwareFlowControl: "HardwareFlowControl",
+    Buffer: "Buffer",
+    Options: "Options",
+    Choose_Usart: "Chose Usart",
+    Open_Usart: "Open Usart",
+    Usart_is_not_open: "Usart is not open",
+}
+
+export default language;
--- src/serial/SerialLeft.tsx
+++ src/serial/SerialLeft.tsx
@@ -13,12 +13,13 @@ import {
   Theme,
 } from "@mui/material";
 import { ChangeEvent, useState } from "react";
-// import language from '../locales/zh'
-import language from '../locales/en'
+import { useTranslation } from "react-i18next";
 
 interface PropsType {}
 
 function SerialLeft(type: PropsType) {
+  const { t } = useTranslation();
+
   // 波特率
   const [baudRate, setBaudRate] = useState("115200");
   const baudRateHandleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -62,14 +63,14 @@ function SerialLeft(type: PropsType) {
 
   return (
     <Card>
-      <CardHeader title={language.Options} subheader={<span>{language["Usart is not open"]}</span>} />
+      <CardHeader title={t('Options')} subheader={<span>{t('Usart_is_not_open')}</span>} />
       {/* 波特率 */}
-      <TextField sx={sxProps} label={language.BaudRate} variant="outlined" onChange={baudRateHandleChange} value={baudRate} />
+      <TextField sx={sxProps} label={t('BaudRate')} variant="outlined" onChange={baudRateHandleChange} value={baudRate} />
 
       {/* 数据位 */}
       <FormControl sx={sxProps}>
-        <InputLabel id="data-bit-label">{language.WordLength}</InputLabel>
-        <Select labelId="data-bit-label" value={dataBit} label={language.WordLength} onChange={dataBitHandleChange}>
+        <InputLabel id="data-bit-label">{t('WordLength')}</InputLabel>
+        <Select labelId="data-bit-label" value={dataBit} label={t('WordLength')} onChange={dataBitHandleChange}>
           <MenuItem value={"8"}>8</MenuItem>
           <MenuItem value={"7"}>7</MenuItem>
         </Select>
@@ -77,8 +78,8 @@ function SerialLeft(type: PropsType) {
 
       {/* 停止位 */}
       <FormControl sx={sxProps}>
-        <InputLabel id="stop-bit-label">{language.StopBits}</InputLabel>
-        <Select labelId="stop-bit-label" value={stopBit} label={language.StopBits} onChange={stopBithandleChange}>
+        <InputLabel id="stop-bit-label">{t('StopBits')}</InputLabel>
+        <Select labelId="stop-bit-label" value={stopBit} label={t('StopBits')} onChange={stopBithandleChange}>
           <MenuItem value={"1"}>1</MenuItem>
           <MenuItem value={"2"}>2</MenuItem>
         </Select>
@@ -86,8 +87,8 @@ function SerialLeft(type: PropsType) {
 
       {/* 校验位 */}
       <FormControl sx={sxProps}>
-        <InputLabel id="check-bit-label">{language.Parity}</InputLabel>
-        <Select labelId="check-bit-label" value={checkBit} label={language.Parity} onChange={checkBithandleChange}>
+        <InputLabel id="check-bit-label">{t('Parity')}</InputLabel>
+        <Select labelId="check-bit-label" value={checkBit} label={t('Parity')} onChange={checkBithandleChange}>
           <MenuItem value={"None"}>None</MenuItem>
           <MenuItem value={"Even"}>Even</MenuItem>
           <MenuItem value={"Odd"}>Odd</MenuItem>
@@ -95,12 +96,12 @@ function SerialLeft(type: PropsType) {
       </FormControl>
 
       {/* 缓冲区 */}
-      <TextField sx={sxProps} label={language.Cache} variant="outlined" onChange={bufferHandleChange} value={buffer} />
+      <TextField sx={sxProps} label={t('Buffer')} variant="outlined" onChange={bufferHandleChange} value={buffer} />
 
       {/* 控制流 */}
       <FormControl sx={sxProps}>
-        <InputLabel id="control-flow-label">{language.HardwareFlowControl}</InputLabel>
-        <Select labelId="control-flow-label" value={controlFlow} label={language.HardwareFlowControl} onChange={controlFlowhandleChange}>
+        <InputLabel id="control-flow-label">{t('HardwareFlowControl')}</InputLabel>
+        <Select labelId="control-flow-label" value={controlFlow} label={t('HardwareFlowControl')} onChange={controlFlowhandleChange}>
           <MenuItem value={"None"}>None</MenuItem>
           <MenuItem value={"HardWare"}>HardWare</MenuItem>
         </Select>
@@ -109,12 +110,12 @@ function SerialLeft(type: PropsType) {
       <Grid container spacing={2} sx={{ p: 1 }}>
         <Grid item xs={6}>
           <Button variant="outlined" fullWidth>
-            {language["Choose Usart"]}
+            {t('Choose_Usart')}
           </Button>
         </Grid>
         <Grid item xs={6}>
           <Button variant="contained" fullWidth>
-            {language["Open Usart"]}
+            {t('Open_Usart')}
           </Button>
         </Grid>
       </Grid>
